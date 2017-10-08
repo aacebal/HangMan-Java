49c24e3b2b82e3a7076023307041347bafb99a47
Adel Acebal <adelacebal@yahoo.com>
2017-10-08 16:20:12 -0400

    game done

---
+++ src/com/adelacebal/Prompter.java
@@ -0,0 +1,41 @@
+package com.adelacebal;
+
+import java.util.Scanner;
+
+public class Prompter {
+    private Game game;
+
+    public Prompter(Game game) {
+        this.game = game;
+    }
+
+    public boolean promptForGuess() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isHit = false;
+        boolean isAcceptable = false;
+
+        do {
+            System.out.print("Enter a letter: ");
+            String guessInput = scanner.nextLine();
+            try {
+                isHit = game.applyGuess(guessInput);
+                isAcceptable = true;
+            } catch (IllegalArgumentException iae) {
+                System.out.printf("%s. Please try again. %n", iae.getMessage());
+            }
+        } while (! isAcceptable);
+        return isHit;
+    }
+
+    public void displayProgress() {
+        System.out.printf("You have %d tries left to solve: %s%n", game.getRemainingTries(), game.getCurrentProgress());
+    }
+
+    public void displayOutcome() {
+        if (game.isWon()) {
+            System.out.println("Congratulations! You Won with " + game.getRemainingTries() + " tries remaining");
+        } else {
+            System.out.println("Bummer! The word was " + game.getAnswer() + ".");
+        }
+    }
+}
